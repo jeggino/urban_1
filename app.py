8b7d7ee2fe5f1d1c0d63065e55d30ee2f810de2f
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-08-05 09:24:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import geopandas as gpd
 import osmnx as ox
-import folium
-
-from streamlit_folium import st_folium
+import pydeck as pdk
 
 
 
@@ -14,9 +12,28 @@ st.set_page_config(
 )
 
 
-m = folium.Map(tiles="CartoDB positron",zoom_start=1)
 
 train_stations = ox.geometries_from_place("Amsterdam", tags={"railway":'station'}).loc["node"]
-m.add_child(train_stations.explore(tooltip=["old_name:1985-2006"]))
 
-st_data = st_folium(m, width=725)
+
+# Define a layer to display on a map
+layer = pdk.Layer(
+    "ScatterplotLayer",
+    train_stations,
+    pickable=True,
+    opacity=0.8,
+    stroked=True,
+    filled=True,
+    radius_scale=3,
+    radius_min_pixels=1,
+    radius_max_pixels=100,
+    line_width_min_pixels=1,
+    get_position="geometry.coordinates",
+    get_radius=100,
+    get_fill_color=[255, 140, 0],
+    get_line_color=[0, 0, 0],
+)
+
+r = pdk.Deck(layers=[layer],tooltip={"text": "{name}"})
+
+st.pydeck_chart(pydeck_obj=f, use_container_width=True)

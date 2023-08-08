104dd1179da794cc29bc78f27580f59dcfa35aae
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-08-08 16:52:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ map = pdk.Deck(layers,
         )
 
 #----------------------------------------------------------------
-right.pydeck_chart(map)
+st.pydeck_chart(map)
 
 
 #----------------------------------------------------------------
@@ -156,6 +156,6 @@ bar = base.mark_bar(
 
 text = base.mark_text(align='left', dx=2)
 
-left.altair_chart(altair_chart=(bar + text), use_container_width=True, theme="streamlit")
+st.sidebar.altair_chart(altair_chart=(bar + text), use_container_width=True, theme="streamlit")
 
 
